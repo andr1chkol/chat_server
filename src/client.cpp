ec31fe9f3220878f2c7f1c53bf209fcdf46cf22e
Andrii Lazaresku <andriylazarescu@gmail.com>
2025-11-10 15:32:28 +0100

    initial setup

---
+++ src/client.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "Chat client starting..." << std::endl;
+    return 0;
+}
---
+++ src/server.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    std::cout << "Chat server starting..." << std::endl;
+    return 0;
+}
